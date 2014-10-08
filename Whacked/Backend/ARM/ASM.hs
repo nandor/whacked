d1f089a51e55ea54635f6735861b3dc9733dbd89
Nandor Licker <licker.nandor@gmail.com>
2014-10-08 16:26:16 +0100

    Compiler monad

--- Whacked/Backend/ARM/ASM.hs
+++ Whacked/Backend/ARM/ASM.hs
@@ -1,5 +1,29 @@
 module Whacked.Backend.ARM.ASM where
 
+
+data ARMReg
+  = R0
+  | R1
+  | R2
+  | R3
+  | R4
+  | R5
+  | R6
+  | R7
+  | R8
+  | R9
+  | R10
+  | R11
+  | R12
+  | SP
+  | LR
+  | PC
+  deriving (Eq, Ord, Show)
+
+
 data ASM
-  = ASM
+  = ARMLabel String
+  | ARMAdd ARMReg ARMReg ARMReg
+  | ARMSub ARMReg ARMReg ARMReg
+  | ARMMul ARMReg ARMReg ARMReg
   deriving (Eq, Ord, Show)
--- Whacked/Backend/ARM/Compiler.hs
+++ Whacked/Backend/ARM/Compiler.hs
@@ -1,7 +1,39 @@
-module Whacked.Backend.ARM.Compiler where
+{-# LANGUAGE RecordWildCards, NamedFieldPuns, GeneralizedNewtypeDeriving #-}
+module Whacked.Backend.ARM.Compiler
+  ( compile
+  ) where
 
-import Whacked.Backend.ARM.ASM
-import Whacked.IMF
+
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.State
+import           Control.Monad.Writer
+import           Whacked.Backend.ARM.ASM
+import           Whacked.IMF
+
+
+
+data Scope
+  = Scope
+    {
+    }
+  deriving (Eq, Ord, Show)
+
+
+newtype Compiler a
+  = Compiler
+    { run :: StateT Scope (Writer [ASM]) a
+    }
+  deriving (Applicative, Functor, Monad, MonadState Scope, MonadWriter [ASM])
+
+
+compileProgram :: IProgram -> Compiler ()
+compileProgram program
+  = return ()
+
 
 compile :: IProgram -> [ASM]
-compile _ = []
+compile program
+  = execWriter . evalStateT (run $ compileProgram program) $ scope
+  where
+    scope = Scope
